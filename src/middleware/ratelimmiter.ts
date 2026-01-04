90a73828d7b814f8e9281c13a725cadc19fb8970
simn9001 <simongtungo300@gmail.com>
2026-01-05 04:25:14 +0800

    naendelea

--- src/middleware/ratelimmiter.ts
+++ src/middleware/ratelimmiter.ts
@@ -1,9 +1,8 @@
 import { rateLimiter } from "hono-rate-limiter";
 
 export const limiter = rateLimiter({
-  windowMs: 60 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 10 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-   keyGenerator: () => "<unique_key>", // Method to generate custom identifiers for clients.
-  // store: ... , // Redis, MemoryStore, etc. 
+  windowMs: 60 * 1000, // 1 minute
+  limit: 500, // Limit each IP to 500 requests per minute
+  standardHeaders: "draft-6",
+  keyGenerator: (c) => c.req.header("x-forwarded-for") || c.req.header("x-real-ip") || "default",
 });
